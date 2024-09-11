474324b21744754c78db9d00aa3e5d5bc6f583ef
Annajirao Challa <Annajiraochalla@gmail.com>
2024-09-11 14:08:54 -0400

    Update

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
-def example_rule():
-    '''GKL Rule'''
-    model = RoboCA(rule = '0x5f005f005f005f005fff5f005fff5f', noise_range = (0.0, 1.0),
-                    noise_interval = 0.002, lattice_size = 149, test_rate = 5, deviations = 3, n_walkers = 100_000)
-    return model.get_robustness()
+import RoboCA as rca
+
+
+# GKL Rule in Hexadecimal Representation
+GKL_HEX = '0x5f005f005f005f005fff5f005fff5f'
+
+
+model = rca(rule = GKL_HEX, noise_range = (0.0, 1.0),
+                noise_interval = 0.002, lattice_size = 149, test_rate = 5, deviations = 3, n_walkers = 100_000)
+return model.get_robustness()
