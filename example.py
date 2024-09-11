7ad5ebf5515899cd4a5667263ce7ee647a5e1b3e
Annajirao Challa <Annajiraochalla@gmail.com>
2024-09-11 14:09:02 -0400

    Update

--- example.py
+++ example.py
@@ -5,6 +5,6 @@ import RoboCA as rca
 GKL_HEX = '0x5f005f005f005f005fff5f005fff5f'
 
 
-model = rca(rule = GKL_HEX, noise_range = (0.0, 1.0),
+model = rca.Model(rule = GKL_HEX, noise_range = (0.0, 1.0),
                 noise_interval = 0.002, lattice_size = 149, test_rate = 5, deviations = 3, n_walkers = 100_000)
 return model.get_robustness()
